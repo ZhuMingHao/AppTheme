4c10e61e188594dd48efef3760b19cd9ef305c0c
Nico Zhu (Shanghai Wicresoft Co <v-zhumin@microsoft.com>
2020-08-14 09:28:46 +0800

    添加项目文件。

---
+++ AppTheme/AppSettings.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace AppTheme
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class AppSettings : Page
+    {
+        public const ElementTheme DEFAULTTHEME = ElementTheme.Light;
+        public const ElementTheme NONDEFAULTHEME = ElementTheme.Dark;
+
+        const string KEY_THEME = "appColourMode";
+        static ApplicationDataContainer LOCALSETTINGS = ApplicationData.Current.LocalSettings;
+
+        /// <summary>
+        /// Gets or sets the current app colour setting from memory (light or dark mode).
+        /// </summary>
+        public static ElementTheme Theme
+        {
+            get
+            {
+                // Never set: default theme
+                if (LOCALSETTINGS.Values[KEY_THEME] == null)
+                {
+                    LOCALSETTINGS.Values[KEY_THEME] = (int)DEFAULTTHEME;
+                    return DEFAULTTHEME;
+                }
+                // Previously set to default theme
+                else if ((int)LOCALSETTINGS.Values[KEY_THEME] == (int)DEFAULTTHEME)
+                    return DEFAULTTHEME;
+                // Previously set to non-default theme
+                else
+                    return NONDEFAULTHEME;
+            }
+            set
+            {
+                // Error check
+                if (value == ElementTheme.Default)
+                    throw new System.Exception("Only set the theme to light or dark mode!");
+                // Never set
+                else if (LOCALSETTINGS.Values[KEY_THEME] == null)
+                    LOCALSETTINGS.Values[KEY_THEME] = (int)value;
+                // No change
+                else if ((int)value == (int)LOCALSETTINGS.Values[KEY_THEME])
+                    return;
+                // Change
+                else
+                    LOCALSETTINGS.Values[KEY_THEME] = (int)value;
+            }
+        }
+
+        public AppSettings()
+        {
+            this.InitializeComponent();
+
+            // Set theme for window root
+            FrameworkElement root = (FrameworkElement)Window.Current.Content;
+            root.RequestedTheme = AppSettings.Theme;
+            SetThemeToggle(AppSettings.Theme);
+        }
+
+        private void SetThemeToggle(ElementTheme theme)
+        {
+            if (theme == AppSettings.DEFAULTTHEME)
+                TglDarkMode.IsOn = false;
+            else
+                TglDarkMode.IsOn = true;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // MainPage.Current.BackButton.Visibility = Visibility.Visible;
+            //  MainPage.Current.BackButton.IsEnabled = true;
+
+            //   MainPage.mAppBarButtonSettings.Visibility = Visibility.Collapsed;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            //  MainPage.mAppBarButtonSettings.Visibility = Visibility.Visible;
+        }
+        public static async Task SetRequestedThemeAsync()
+        {
+            foreach (var view in CoreApplication.Views)
+            {
+                await view.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    if (Window.Current.Content is FrameworkElement frameworkElement)
+                    {
+                        frameworkElement.RequestedTheme = Theme;
+                    }
+                });
+            }
+        }
+
+        private void TglDarkMode_Toggled(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement window = (FrameworkElement)Window.Current.Content;
+
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    AppSettings.Theme = AppSettings.NONDEFAULTHEME;
+                    window.RequestedTheme = AppSettings.NONDEFAULTHEME;
+                }
+                else
+                {
+                    AppSettings.Theme = AppSettings.DEFAULTTHEME;
+                    window.RequestedTheme = AppSettings.DEFAULTTHEME;
+                }
+            }
+        }
+    }
+}
---
+++ AppTheme/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了"空白页"项模板
+
+namespace AppTheme
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AppSettings));
+           
+          
+        }
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            byte[] byteNew = md5.ComputeHash(byteOld);
+           
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in byteNew)
+            {
+                
+                sb.Append(b.ToString("x2"));
+            }
+           
+            return sb.ToString();
+        }
+        public string CreateRandCdkeys(int x)
+        {
+            string[] codeSerial = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            Random rand = new Random();
+            int temp = -1;
+            string cdKey = string.Empty;
+            for (int i = 0; i < 16; i++)
+            {
+                if (temp != -1)
+                {
+                    rand = new Random(x + i * temp * unchecked((int)DateTime.Now.Ticks));
+                }
+                int randIndex = rand.Next(0, 35);
+                temp = randIndex;
+                cdKey += codeSerial[randIndex];
+            }
+            return cdKey;
+        }
+
+    }
+
+
+    public delegate Task<int> PrintCaller([CallerMemberName] string Caller = null);
+    public class MyClass
+    {
+        public async Task<string> InitiateAPICallAsync(PrintCaller apiCall)
+        {
+            var response = await apiCall();
+            return "Test";
+        }
+
+        public async void MyFunc()
+        {
+            var helper = new APIHelper();
+            var str1 = await InitiateAPICallAsync(new PrintCaller(helper.GetData));
+            var str2 = await helper.GetData();
+        }
+       
+
+
+    }
+    public class APIHelper
+{
+    public void Test(string p)
+    {
+
+    }
+    public async Task<int> GetData([CallerMemberName] string Caller = null)
+    {
+
+        if (Caller == "InitiateAPICallAsync")
+        {
+            // do some thing
+        }
+        else
+        {
+            //Show Warning
+            var dialog = new MessageDialog("Waring!!! Please don't call it directly");
+            await dialog.ShowAsync();
+        }
+
+        return 0;
+    }
+}
+
+
+
+public class Response1 : BaseResponse { }
+
+public class Response2 : BaseResponse { }
+
+public class BaseResponse { }
+}
+
+
